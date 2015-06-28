b07dad03f83ff789962e30629aeeb88a0e23aa69
Thomas Shelby <mikola1693@gmail.com>
2015-06-28 16:25:20 +0300

    Collection_vs_Map

--- src/DoIt.java
+++ src/DoIt.java
@@ -55,7 +55,7 @@ public class DoIt{
 	}
 }*/
 
-import java.util.*;
+/*import java.util.*;
 
 public class DoIt{
 	public static void main(String[] args) {
@@ -64,6 +64,37 @@ public class DoIt{
 			c.add(i);
 		for(Integer i:c)
 			System.out.print(i+", ");
+	}
+}*/
+
+//====================================================================================================
+//===================================___COLLECTION_VS_MAP___==========================================
+import java.util.*;
+
+public class DoIt{
+	static Collection fill(Collection<String> collection){
+		collection.add("rat");
+		collection.add("cat");
+		collection.add("dog");
+		collection.add("dog");
+		return collection;
+	}
+	static Map fill(Map<String,String> map){
+		map.put("rat", "Fuzzy");
+		map.put("cat","Rags");
+		map.put("dog", "Bosco");
+		map.put("dog", "Spot");
+		return map;
+	}
+	public static void main(String[] args) {
+		System.out.println(fill(new ArrayList<String>()));
+		System.out.println(fill(new LinkedList<String>()));
+		System.out.println(fill(new HashSet<String>()));
+		System.out.println(fill(new TreeSet<String>()));
+		System.out.println(fill(new LinkedHashSet<String>()));
+		System.out.println(fill(new HashMap<String,String>()));
+		System.out.println(fill(new TreeMap<String,String>()));
+		System.out.println(fill(new LinkedHashMap<String,String>()));
 	}
 }
 
